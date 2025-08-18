c0d19ba495b0a9d23d6c607e43d8b2e78c491461
iiiRyan <85ryan.blue@gmail.com>
2025-08-18 21:03:12 +0800

    fix: 测试数据缓存策略

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -12,19 +12,16 @@ export const client = createClient(config);
 export async function sanityFetch<QueryResponse>({
   query,
   params = {},
-  revalidate = 604800, // 缓存 1 周
   tags = [],
 }: {
   query: string
   params?: QueryParams
-  revalidate?: number | false
   tags?: string[]
 }): Promise<QueryResponse> {
   return client.fetch<QueryResponse>(query, params, {
     // 在 Next.js 15 中，必须明确设置 cache 策略
     // cache: 'force-cache',
     next: {
-      revalidate: tags.length ? false : revalidate,
       tags, // 将 tags 传递给 Next.js 的 fetch
     },
   })
